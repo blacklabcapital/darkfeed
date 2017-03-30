c41cea831ddf5443a93681d26b0d5f1dbbeaee69
Alfredo Luque <me@aluque.io>
2017-03-29 23:06:36 -0400

    [cpp] Add test for hardware accelerated (SSE4.3) CRC32C symbol hasher

--- cpp/tests/hashing_test.cpp
+++ cpp/tests/hashing_test.cpp
@@ -61,3 +61,23 @@ TEST(CRC32CStrHasher, Alphabet)
     std::uint32_t crc = hasher(alphabet);
     ASSERT_EQ(*(std::uint32_t *) expected_arr, crc);
 }
+
+
+TEST(CRC32CSymHasher, BaseCase)
+{
+    Symbol s(MIC::XNYS, "BRK.PR.A");
+    ASSERT_STREQ("BRK", s.root);
+    ASSERT_EQ(IssueType::preferred, s.issue_type);
+    ASSERT_EQ('A', s.series);
+    CRC32CSymHasher h;
+    auto h1 = h(s);
+    auto sc = s;
+    auto h2 = h(sc);
+    ASSERT_EQ(h1, h2);
+    Symbol s2(MIC::XNYS, "BRK.A");
+    ASSERT_STREQ("BRK", s2.root);
+    ASSERT_EQ(IssueType::normal, s2.issue_type);
+    ASSERT_EQ('A', s2.series);
+    auto h3 = h(s2);
+    ASSERT_NE(h1, h3);
+}
